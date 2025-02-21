a585c1fbe0e424995eff41fb29980cda803823ee
John Wesly P D <112474505+johnwesly08@users.noreply.github.com>
2025-02-21 16:11:30 +0530

    Update Basics_1.sol

--- Basics_1.sol
+++ Basics_1.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity >=0.7.0 <0.9.0;
 
+// Hello World
+
 contract intro {
-    string public str="Hello World";//printing a hello world
+    string public str="Hello World"; //printing a hello world
 }
+
+// Data types
 
 contract DataTypes {
     //Types
@@ -15,40 +19,32 @@ contract DataTypes {
 }
 
 //declaring a state variable 
-// uint => Unsigned Integer
 
 contract variable {
-    uint public num = 5;//state variable
+    uint public num = 5; //state variable
     constructor() {
         num = 10;
     }
 
-//     /*Always declaring any variable or function along the Public keyword will make those functionality to be open and public */
-//     /*Generally all the variables that is declared inside the contract outside the function is said to be the State Variable. Meanwhile, this state variable will be usually in the permanent memory like storage in storage contract */
-    
-    // uint public num;// By declaring a variable with no value will gives the output value as Zero(Which is a default)
-
-    //declaring a function
+//declaring a function
     function seter() public {
         num = 200;
     }
 }
-// // Thus, to change the dafault values of the state variable, we can use the above methods => 1. constructor, 2.Setter Function,3. Or by just change the initializing of a variable
 
 contract _variable {
-
-    uint c;//State variable
+    uint c; //State variable
     function local() public pure returns(uint) {
-        uint a;//Local Variable
-        uint b;//Local Variable
+        uint a; //Local Variable
+        uint b; //Local Variable
         return a+b;
-    }/*There are actually 3 areas namely, 1. Stack, 2. Storage, 3. Memory */
+    }
 }
 
-// how to create a function
+// Creating a function
 contract func_1 {
     uint public num1; //State variable
-    uint abc;// it is not a public variable
+    uint abc; // it is not a public variable
 
     function setter(uint _num) public {
         num1 = _num;
@@ -60,6 +56,10 @@ contract func_1 {
     }
 }
 
+// Always declaring any variable or function along the Public keyword will make those functionality to be open and public.
+// Generally all the variables that is declared inside the contract outside the function is said to be the State Variable. Meanwhile, this state variable will be usually in the permanent memory like storage in storage contract.  
+// By declaring a variable with no value will gives the output value as Zero(Which is a default)
+// Thus, to change the dafault values of the state variable, we can use the above methods => 1. constructor, 2.Setter Function,3. Or by just change the initializing of a variable
 //important points for functions
 /*
     when setter function is called and used, it creates a transaction and costs a gas, becoz it changes the blockChain
