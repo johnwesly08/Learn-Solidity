448b65b6abe2197b7966e0c8376d18bc323e00f1
John Wesly P D <112474505+johnwesly08@users.noreply.github.com>
2025-02-21 16:04:43 +0530

    Update Arrays.sol

--- contracts/Arrays.sol
+++ contracts/Arrays.sol
@@ -1,77 +1,61 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.21;
 
-// // contract myArrays {
-// //     uint256[] public array1 = [1, 2, 3, 4, 5, 6, 7];
+// Basic Array declaration, retriving and acccessing elements 
 
-// //     function arraycall() public view returns (uint256[]memory) {
-// //             return array1;
-// //         }
-// //     function getMyArray(uint _i) public view returns(uint256) {
-// //         require(_i < array1.length);
-// //         return array1[_i];
-// //     }
-// // }
+contract myArrays {
+     uint256[] public array1 = [1, 2, 3, 4, 5, 6, 7];
 
-// contract array_operation{
-//     uint[] myArray = [1,2,3];
-//     function toAdd(uint _num) public{
-//         myArray.push(_num);
-//     }
+    function arraycall() public view returns (uint256[]memory) {
+            return array1;
+        }
 
-//     // function toPop(uint _num) public{
-//     //     myArray.pop(_num);
-//     // }
+    function getMyArray(uint _i) public view returns(uint256) {
+        require(_i < array1.length);
+        return array1[_i];
+    }
+}
+
+// Static Array
+
+contract static_array {
+    function myArr() public pure returns(int[5] memory) {
+        int[5] memory data = [int(50),-63,77,-28,90];
+        return data;
+    }
+}
+
+// Dyanmic arrays 
+
+contract dynamicArryas {
+    uint[] data = [1,2,3,4,5]; //static array
+    int[] data1; 
+    function dyArr() public returns(uint[] memory,int[] memory) {
+        data1 = [int(-68),878,89,7,45,64,57];
+        return (data,data1);
+    }
+}
+
+// Array Operations
+
+contract arrayOperation{
+    uint[] myArray = [1,2,3];
+    function toAdd(uint _num) public{
+        myArray.push(_num);
+    }
+
+    function toPop() public{
+        require(myArray.length > 0, "Array is empty");
+        myArray.pop();
+    }
  
-//     function toDel(uint _num) public{
-//         delete myArray[_num];
-//     }
-
+    function toDel(uint _num) public{
+        require(_num < myArray.length, "Index");
+        delete myArray[_num];
+    }
     
-//     function showTodo() public view returns(uint[] memory){
-//         return myArray;
-//     }
+    function showTodo() public view returns(uint[] memory){
+        return myArray;
+    }
+} 
 
-// } 
-
-// contract static_array {
-//     function myArr() public pure returns(int[5] memory) {
-//         int[5] memory data = [int(50),-63,77,-28,90];
-//         return data;
-//     }
-// }
-
-// // Dyanmic arrays 
-// contract dynamicArryas {
-//     uint[]  data = [1,2,3,4,5]; //static array
-//     int[] data1; 
-//     function dyArr() public returns(uint[] memory,int[] memory) {
-//         data1 = [int(-68),878,89,7,45,64,57];
-    
-//         return (data,data1);
-//     }
-// }
-
-// //array operations 
-// contract access {
-//     uint[]  data = [1,2,3,4,5]; //static array
-//     int[] data1; 
-//     function dyArr() public returns(int) {
-//         data1 = [int(-68),878,89,7,45,64,57];
-
-//         int x = data1[2];
-    
-//         return x;
-//     }
-// }
-
-// SImilaly firgettoing the leangth of the Array use
-// <dataType> <variable_name> = <Array_declared_variable>.length
-
-// contract slice { 
-//     uint[] MyArray = [1,2,3,4,5];
-//     function myArray() public pure returns(int[] memory) {
-//         int[] memory Slice = slice(MyArray,0,2);
-//         return Slice;
-//     }
-// Not yet confirmed
