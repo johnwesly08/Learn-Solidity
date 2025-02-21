0a47032182cf9eed6c7863a8401479e7b474828f
John Wesly P D <112474505+johnwesly08@users.noreply.github.com>
2025-02-21 16:18:37 +0530

    Update BASICS_2.sol

--- contracts/BASICS_2.sol
+++ contracts/BASICS_2.sol
@@ -1,16 +1,14 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 < 0.9.0;
 
-// //using of pure and view keyword
+// pure and view keyword
 contract vp {
     uint public num1;
-
     function setter(uint _num) public{
         num1 = _num;
     }
 
-//     //I am just reading the state variable, and hence => View{Keyword}
+// view => Read the state variable
     function getter() public view returns(uint) {
         return num1;
     }
@@ -21,27 +19,27 @@ contract vp {
     }
 }
 
+//Constructors
+
+contract construct {
+    uint public num=5;
+    constructor(uint _num){
+        num=_num;
+    }
+}
+
+contract intData {
+    uint public num=1;
+    int public _num = -12;
+    int8 value = 128; // error
+}
+
 // /* 
 // Points to be noted
 // 1. When we are just changing the values of the state variable, there's ain't no need for pure or view
 // 2. I am just reading the state variable, and hence => View{Keyword}
 // 3. Whenever you're modifying the local variable, either reading,computing or writing a local variable,then use the pure keyword.
 // */
-
-
-
-
-
-// //Constructors
-
-// contract construct {
-//     uint public num=5;
-
-//     constructor(uint _num){
-//         num=_num;
-//     }
-
-// }
 
 // /*
 //     According to solidity, these constructors are also called as functions.
@@ -50,18 +48,6 @@ contract vp {
 //     Even you haven't created a cinstrutor in the code, the Solidity will itself will create a constructor. 
 //  */
 
-//Day two
-
-
 //Integer data type
 //int holds both negative and positive numbers, while uint holds positive numbers only. the usual default value is zero.
 //range calculation that int hold => 2^(n-1) to 2^(n-1)-1; for uint => 0 to 2^(n-1)-1;
-
-
-// contract intData {
-//     uint public num=1;
-//     int public _num = -12;
-//     // int8 value = 128;//-128 to 127
-    
-// }
-
